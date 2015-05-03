e1dd6a1a10e0278d1448357400ee5c903186346d
Josh Arnold <joshuaarnold22@gmail.com>
2015-05-03 11:16:49 -0400

    Completed seed data assignment

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,21 +1,30 @@
 require 'faker'
  
  # Create Posts
- 50.times do
+
+  50.times do
    Post.create!(
      title:  Faker::Lorem.sentence,
      body:   Faker::Lorem.paragraph
    )
- end
+  end
+
  posts = Post.all
+
+ unique_post = [{title: 'This is a new unique title', body: 'This is a new unique body'}] #why can't I place this in the first Post.create!? & what is [{}] about?
+
+ unique_post.each do |attributes|
+    Post.create(attributes) unless Post.where(attributes).first
+  end
  
  # Create Comments
- 100.times do
+
+  100.times do
    Comment.create!(
      post: posts.sample,
      body: Faker::Lorem.paragraph
    )
- end
+  end
  
  puts "Seed finished"
  puts "#{Post.count} posts created"
