28c203cfd67d85bd135aa87f1962539bb27b1a06
Josh Arnold <joshuaarnold22@gmail.com>
2015-05-08 08:21:50 -0400

    Completed the CRUD checkpoint

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,8 @@
-Rails.application.routes.draw do
-  get 'welcome/contact'
+Bloccit::Application.routes.draw do
 
-  get 'welcome/index'
+  resources :posts
 
-  get 'welcome/about'
+  get 'about' => 'welcome#about'
 
   root to: 'welcome#index'
 end
