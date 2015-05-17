70e60083b47003d0809f08f5e8fed662cc201e0a
Josh Arnold <joshuaarnold22@gmail.com>
2015-05-17 15:48:52 -0400

    Completed Associations Assignment

--- db/seeds.rb
+++ db/seeds.rb
@@ -43,6 +43,7 @@ require 'faker'
 
   100.times do
    Comment.create!(
+    # user: users.sample,   # we have not yet associated Users with Comments
      post: posts.sample,
      body: Faker::Lorem.paragraph
    )
