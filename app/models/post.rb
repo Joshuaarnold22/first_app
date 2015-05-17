947045f7e9b354b74f45264204482ab5ad6fee52
Josh Arnold <joshuaarnold22@gmail.com>
2015-05-17 15:20:16 -0400

    Completed Associations Checkpoint

--- app/models/post.rb
+++ app/models/post.rb
@@ -7,8 +7,12 @@
 #  body       :text
 #  created_at :datetime         not null
 #  updated_at :datetime         not null
+#  user_id    :integer
 #
 
 class Post < ActiveRecord::Base
   has_many :comments
+  belongs_to :user
+
+  default_scope { order('created_at DESC') }
 end
