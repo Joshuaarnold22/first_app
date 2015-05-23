4e8b8f3fc8e8cc337ac0db78bb090a022a4849b6
Josh Arnold <joshuaarnold22@gmail.com>
2015-05-23 12:39:22 -0400

    Configured app for Prodcution emails

--- app/policies/post_policy.rb
+++ app/policies/post_policy.rb
@@ -1,14 +1,14 @@
 class PostPolicy < ApplicationPolicy
-  class Scope < Scope
+  # class Scope < Scope
 
-    def resolve
-      if !user.blank? && (user.admin? || user.moderator?) # Why not if user.admin? || user.moderator?
-        scope.all
-      else
-        scope.where(user: user) # user: user ?
-      end
-    end
-  end
+  #   def resolve
+  #     if !user.blank? && (user.admin? || user.moderator?) # Why not if user.admin? || user.moderator?
+  #       scope.all
+  #     else
+  #       scope.where(user: user) # user: user ?
+  #     end
+  #   end
+  # end
   def index?
     true
   end
