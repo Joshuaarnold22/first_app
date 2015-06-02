7fa3c7a622580db1d8e283380ab14ec6645d2d35
Josh Arnold <joshuaarnold22@gmail.com>
2015-06-02 18:26:55 -0400

    Assignment-36-Comments

--- app/policies/comment_policy.rb
+++ app/policies/comment_policy.rb
@@ -1,10 +1,9 @@
- class CommentPolicy < ApplicationPolicy
- 
-   def create?
-     user.present?
-   end
- 
-   def update?
-     create?
-   end
- end
+class CommentPolicy < ApplicationPolicy
+  def new
+    user.present?
+  end
+
+  def create?
+    user.present?
+  end
+end
