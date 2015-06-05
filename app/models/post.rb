407c2322ebaa0d4aea9c62f90d377a37411ca5f7
Josh Arnold <joshuaarnold22@gmail.com>
2015-06-05 14:28:49 -0400

    stopping point

--- app/models/post.rb
+++ app/models/post.rb
@@ -15,6 +15,7 @@
 
 class Post < ActiveRecord::Base
   has_many :comments, dependent: :destroy
+  has_many :votes, dependent: :destroy
   belongs_to :user
   belongs_to :topic
   mount_uploader :image, ImageUploader
@@ -23,6 +24,18 @@ class Post < ActiveRecord::Base
 
   validates :title, length: { minimum: 5 }, presence: true
   validates :body, length: { minimum: 20 }, presence: true
-  validates :topic, presence: true
-  validates :user, presence: true
+  #validates :topic, presence: true
+  #validates :user, presence: true
+
+  def up_votes
+    votes.where(value: 1).count
+  end
+
+  def down_votes
+    votes.where(value: -1).count
+  end
+
+  def points
+    up_votes - down_votes
+  end
 end
--- spec/models/vote_spec.rb
+++ spec/models/vote_spec.rb
@@ -1,17 +1,17 @@
-require 'rails_helper'
+require 'rails_helper' #when I created the Vote model I got a conflict with this file
 
 describe Vote do 
   describe "validations" do
     describe "value validation" do
       it "only allows -1 or 1 as values" do
-        @vote_01 = Vote.new(value: 1)
-        expect ( @vote_01.valid?).to eq(true)
+        v1 = Vote.new(value: 1)
+        expect(v1.valid?).to eq(true)
 
-        @vote_02 = Vote.new(value: -1)
-        expect ( @vote_02.valid?).to eq(true)
+        v2 = Vote.new(value: -1)
+        expect(v2.valid?).to eq(true) #valid?
 
-        @vote_03 = Vote.new(value: 2)
-        expect ( @vote_03.valid?).to eq(false)
+        wrong_v = Vote.new(value: 2)
+        expect(wrong_v.valid?).to eq(false)
       end
     end
   end
