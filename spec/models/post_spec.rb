597cf49d0bc034caa7806df0092448f7d94ecc3a
Josh Arnold <joshuaarnold22@gmail.com>
2015-06-07 14:18:12 -0400

    in process

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -20,7 +20,7 @@ describe Post do
   describe "vote methods" do
 
     before do
-      @post = Post.create( title: 'post title', body: 'post bodies must be pretty long.' )
+      @post = associated_post
       3.times { @post.votes.create(value: 1) }
       2.times { @post.votes.create(value: -1) }
     end
@@ -41,6 +41,25 @@ describe Post do
       it "returns the sum of all down and up votes" do
         expect( @post.points ).to eq(1) #where does 'points' come from.
       end
+    end
+
+    def associated_post(options={})
+      post_options = {
+        title: 'Post title',
+        body: 'Post bodies must be pretty long.',
+        topic: Topic.create(name: 'Topic name'),
+        user: authenticated_user
+      }.merge(options)
+
+      Post.create(post_options)
+    end
+
+    def authenticated_user(options={})
+      user_options = {email: "email#{rand}@fake.com", password: 'password'}.merge(options)
+      user = User.new(user_options)
+      user.skip_confirmation!
+      user.save
+      user
     end
   end  
 end
