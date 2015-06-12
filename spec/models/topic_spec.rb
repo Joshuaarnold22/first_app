dddf4cfe8da28cab9bf5d4be06747e2615325e91
Josh Arnold <joshuaarnold22@gmail.com>
2015-06-12 13:47:01 -0400

    Assignment-52-Private

--- spec/models/topic_spec.rb
+++ spec/models/topic_spec.rb
@@ -16,6 +16,7 @@
      before do 
        @public_topic = Topic.create # default is public
        @private_topic = Topic.create(public: false)
+       @all_if_user = Topic.all if user.present?
      end
  
      describe "publicly_viewable" do
@@ -26,18 +27,19 @@
  
      describe "privately_viewable" do
        it "returns a relation of all private topics" do
-         # Your code here
+         expect(Topic.privately_viewable).to eq( [@private_topic] )
        end
      end
  
      describe "visible_to(user)" do
        it "returns all topics if the user is present" do
          user = true # sneaky solution; we don't need a real user, just something truthy
-         # Your code here
+         expect(Topic.visible_to).to eq( [@all_if_user] )
        end
  
        it "returns only public topics if user is nil" do
-         # Your code here
+         user = nil # sneaky solution; we don't need a real user, just something truthy
+         expect(Topic.visible_to).to eq( [@public_topic] )
        end
      end
    end
