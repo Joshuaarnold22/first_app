f3d37a6c29bfa6465ff77afc2b56fddeda0b07fc
Josh Arnold <joshuaarnold22@gmail.com>
2015-06-13 21:16:36 -0400

    Assignment-52-Interlude

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -13,4 +13,20 @@ module ApplicationHelper
     redcarpet = Redcarpet::Markdown.new(renderer, extensions)
     (redcarpet.render markdown).html_safe
   end
+
+  def up_vote_link_classes(post)
+    up = "glyphicon glyphicon-chevron-up"
+    vote = current_user.voted(post) #how do I get current_user.voted?
+    if vote && vote.up_vote?
+      up += "Voted"
+    end
+  end
+
+  def down_vote_link_classes(post)
+    down = "glyphicon glyphicon-chevron-down"
+    vote = current_user.voted(post)
+    if vote && vote.down_vote?
+      down += "Voted"  #why connect to down with "Voted"?
+    end
+  end
 end
