82961de109b172d010e898fce80a82ca26d96612
Josh Arnold <joshuaarnold22@gmail.com>
2015-06-23 20:06:04 -0400

    assignment-51-favoriting

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -30,10 +30,11 @@ require 'rails_helper'
 describe User do
   include TestFactories
 
-  describe "#{favorited(post)}" do
+  describe "favorited post" do
     before do
       @user = authenticated_user #where is authenticated_user comming from?
       @post = associated_post #where is associated_post comming from?
+      @post2 = associated_post2
     end
 
     it "returns `nil` if the user has not favorited the post" do
@@ -42,11 +43,12 @@ describe User do
 
     it "returns the appropriate favorite if it exists" do
       favorite = @user.favorites.where(post: @post).create
-      expect( @user.favorited.(@post) ).to eq(favorite)
+      expect( @user.favorited(@post) ).to eq(favorite)
     end
 
-    # it "returns `nil` if the user has favorited another post" do
-    #   expect( @user.favorited.(@post) ).to be_nil
-    # end
+    it "returns `nil` if the user has favorited another post" do
+      favorite = @user.favorites.where(post: @post2).create
+      expect( @user.favorited(@post) ).to be_nil
+    end
   end
 end
--- spec/support/test_factories.rb
+++ spec/support/test_factories.rb
@@ -9,6 +9,16 @@ module TestFactories
 
     Post.create(post_options)
   end
+  def associated_post2(options={})
+    post_options = {
+      title: 'Post title',
+      body: 'Post bodies must be pretty long.',
+      topic: Topic.create(name: 'Topic name'),
+      user: authenticated_user
+    }.merge(options)
+
+    Post.create(post_options)
+  end
 
   def authenticated_user(options={})
     user_options = {email: "email#{rand}@fake.com", password: 'password'}.merge(options)
